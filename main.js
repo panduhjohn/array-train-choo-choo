fee7ef5937c71c3c7fb79ef18983eef0a352cb9c
Colin Jaffe <balloonasaurus@gmail.com>
2019-07-23 12:55:16 -0400

    Fixes challenge number inconsistencies.

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ console.log('### Challenge #1');
 // Console out each number in the `numbers` array, individually.
 
 
-console.log('### Challenge #4');
+console.log('### Challenge #2');
 
 // Now do the same thing, but backwards.
 
 
-console.log('### Challenge #2');
+console.log('### Challenge #3');
 
 // Console out each number plus 19, individually.
 
@@ -33,36 +33,36 @@ console.log('### Challenge #4');
 // Console out the sum of all the numbers.
 
 
-console.log('### Challenge #3');
+console.log('### Challenge #5');
 
 // Console out each number individually, but only if it's an even number.
 
 
-console.log('### Challenge #4');
+console.log('### Challenge #6');
 
 // Console out each number, but only if it ends with a 4 digit.
 
 
-console.log('### Challenge #4');
+console.log('### Challenge #7');
 
 // Create a _new_ array and, using a loop, fill it with every number from the original array. Then console out the new array in one `console.log`.
 
 
-console.log('### Challenge #4');
+console.log('### Challenge #8');
 
 // Create a _new_ array and, using a loop, fill it with every item from the original array, but in reverse order. Then console out the new array in one `console.log`.
 
 
-console.log('### Challenge #4');
+console.log('### Challenge #9');
 
 // Create a _new_ array and, using a loop, fill it with every odd-numbered item from the original array. Then console out the new array in one `console.log`.
 
 
-console.log('### Challenge #4');
+console.log('### Challenge #10');
 
 // Create a _new_ array and, using a loop, fill it with every number from the original array plus 1. Then console out the new array in one `console.log`.
 
 
-console.log('### Challenge #4');
+console.log('### Challenge #11');
 
-// Create a _new_ array and, using a loop, fill it with every number from the original array, _followed by_, at the next index, the string, ' is one of the best numbers, objectively and obviously.' That is, the first item in the array should be the number 3, followed by the aforementioned string, followed by the number 24, followed by the string again.g
+// Create a _new_ array and, using a loop, fill it with every number from the original array, _followed by_, at the next index, the string, ' is one of the best numbers, objectively and obviously.' That is, the first item in the array should be the number 3, followed by the aforementioned string, followed by the number 24, followed by the string again.
